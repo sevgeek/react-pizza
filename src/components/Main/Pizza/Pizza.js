05e147e0145c651d3b5e50cba9a05898e116c8ea
Dmitry Shmatok <dev@sevgeek.ru>
2020-08-18 18:39:17 +0300

    Edit store structure: pizzas

--- src/components/Main/Pizza/Pizza.js
+++ src/components/Main/Pizza/Pizza.js
@@ -3,21 +3,19 @@ import React from 'react'
 const Pizza = ({ pizzas }) => {
 	return (
 		<React.Fragment>
-			{renderPizzas(pizzas.meaty, pizzas[0])}
-			{renderPizzas(pizzas.chesee, pizzas[1])}
-			{renderPizzas(pizzas.spicy, pizzas[3])}
+			{renderPizzas(pizzas)}
 		</React.Fragment>
 	)
 }
 
-const renderPizzas = (data, type) => data.map((pizza, index) => (
+const renderPizzas = data => data.map(({type, img, title, content, price}, index) => (
 	<div className='col-1'>
 		<div key={`${type}_${index}`} className='pizza'>
-			<img className='pizza-img mb-s' alt={pizza.title} src={pizza.img} />
-			<p className='pizza-title txt-l mb-s'>{pizza.title}</p>
-			<p className='pizza-content txt-m mb-xl'>{pizza.content.join(', ')}</p>
+			<img className='pizza-img mb-s' alt={title} src={img} />
+			<p className='pizza-title txt-l mb-s'>{title}</p>
+			<p className='pizza-content txt-m mb-xl'>{content.join(', ')}</p>
 			<div className='flexbox flex-justify_between flex-align-items_center'>
-				<p className='pizza-price flex-item txt-l'>от {pizza.price} ₽</p>
+				<p className='pizza-price flex-item txt-l'>от {price} ₽</p>
 				<button className='pizza-order flex-item'>Выбрать</button>
 			</div>
 		</div>
