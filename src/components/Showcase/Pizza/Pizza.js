523b97474a2ba868d043f67b4916ac5de9c07a6a
Dmitry Shmatok <dev@sevgeek.ru>
2020-08-20 15:34:44 +0300

    Render pizzas

--- src/components/Showcase/Pizza/Pizza.js
+++ src/components/Showcase/Pizza/Pizza.js
@@ -13,18 +13,18 @@ const Pizza = ({ pizzas, selectedPizzaType = 'all', selectProps }) => (
 	<React.Fragment>
 		{/* Делаем проверку выбора пользователя */}
 		{selectedPizzaType === 'all'
-			? renderPizzas(pizzas, selectProps)
-			: renderPizzas((pizzas.filter((pizza) => pizza.type === selectedPizzaType)), selectProps)}
+			? renderAllPizzas(pizzas, selectProps)
+			: renderPizzas(pizzas[selectedPizzaType], selectProps)}
 	</React.Fragment>
 )
 
 /**
- * @name renderPizzas
- * @description Функция рендера массива пицц
+ * @name renderAllPizzas
+ * @description Функция рендера массива всех пицц
  * @param {Array} data Массив объектов: пицц
  * @param {Function} selectProps Функция выбора свойства пиццы
  */
-const renderPizzas = (data, selectProps) => Object.values(data).map((type) => type.map(({ id, type, title, img, content, config, price }) => (
+const renderAllPizzas = (data, selectProps) => Object.values(data).map((type) => type.map(({ id, type, title, img, content, config, price }) => (
 	<div className='col-1' key={`${type}${id}`}>
 		<div className='pizza'>
 			<img className='pizza-img mb-s' alt={title} src={img} />
@@ -45,6 +45,34 @@ const renderPizzas = (data, selectProps) => Object.values(data).map((type) => ty
 		</div>
 	</div>
 )))
+
+/**
+ * @name renderPizzas
+ * @description Функция рендера массива определённого типа пиццы
+ * @param {Object} data Массив объектов: пицц
+ * @param {Function} selectProps Функция выбора свойства пиццы
+ */
+const renderPizzas = (data, selectProps) => (data).map(({ id, type, title, img, content, config, price }) => (
+	<div className='col-1' key={`${type}${id}`}>
+		<div className='pizza'>
+			<img className='pizza-img mb-s' alt={title} src={img} />
+			<p className='pizza-title txt-l mb-s'>{title}</p>
+			<p className='pizza-content txt-m mb-s'>{firstLetterToUpperCase(content.join(', '))}</p>
+
+			{/* Render pizza config's */}
+			<PizzaProps
+				config={config}
+				pizzaIndex={id}
+				selectProps={(props) => selectProps(props)}
+			/>
+
+			<div className='grid grid grid-2-col grid-row-gap-0 grid-col-gap-xs'>
+				<p className='pizza-price col-1 txt-l col-align-self_center'>{price} ₽</p>
+				<button className='pizza-order col-1'>Выбрать</button>
+			</div>
+		</div>
+	</div>
+))
 
 /**
  * @name firstLetterToUpperCase
