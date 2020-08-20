2bb30ab894b265c84de8fe9bc501e5ac3fa75b5c
Dmitry Shmatok <dev@sevgeek.ru>
2020-08-20 08:44:08 +0300

    Rename render functions in PizzaProps

--- src/components/Showcase/Pizza/PizzaProps/PizzaProps.js
+++ src/components/Showcase/Pizza/PizzaProps/PizzaProps.js
@@ -14,25 +14,25 @@ const PizzaProps = ({ config, pizzaIndex, selectConfigItem }) => (
 		<section className='mb-s'>
 			<div className='mb-xs'>
 				<div className='pizza-config grid grid-3-col grid-row-gap-0 grid-col-gap-xs'>
-					{renderSizeConfigForPizza(props, config, pizzaIndex, selectConfigItem)}
+					{renderSizeProps(props, config, pizzaIndex, selectConfigItem)}
 				</div>
 			</div>
 			<div className='pizza-config grid grid-2-col grid-row-gap-0 grid-col-gap-xs'>
-				{renderDoughConfigForPizza(props, config, pizzaIndex, selectConfigItem)}
+				{renderDoughProps(props, config, pizzaIndex, selectConfigItem)}
 			</div>
 		</section>
 	</React.Fragment>
 )
 
 /**
- * @name renderSizeConfigForPizza
+ * @name renderSizeProps
  * @description Преобразование размера для пиццы, исходя из стандартных значений
  * @param {Object} props Описание всех характеристик для пиццы
  * @param {Object} config Характеристики пиццы из store
  * @param {Number} pizzaIndex Порядковый номер (индекс) пиццы
  * @param {Function} callbackFunction Call-back function: нажатие на характеристику
  */
-const renderSizeConfigForPizza = (props, config, pizzaIndex, callbackFunction) => props.size.map(({ size, alias }) => {
+const renderSizeProps = (props, config, pizzaIndex, callbackFunction) => props.size.map(({ size, alias }) => {
 	// Определяем активную характеристику
 	const selected = config.size === size ? true : false
 	return (
@@ -44,14 +44,14 @@ const renderSizeConfigForPizza = (props, config, pizzaIndex, callbackFunction) =
 })
 
 /**
- * @name renderDoughConfigForPizza
+ * @name renderDoughProps
  * @description Преобразование теста для пиццы, исходя из стандартных значений
  * @param {Object} props Описание всех характеристик для пиццы
  * @param {Object} config Характеристики пиццы из store
  * @param {Number} pizzaIndex Порядковый номер (индекс) пиццы
  * @param {Function} callbackFunction Call-back function: нажатие на характеристику
  */
-const renderDoughConfigForPizza = (props, config, pizzaIndex, callbackFunction) => props.dough.map(({ dough, alias }) => {
+const renderDoughProps = (props, config, pizzaIndex, callbackFunction) => props.dough.map(({ dough, alias }) => {
 	// Определяем активную характеристику
 	const selected = config.dough === dough ? true : false
 	return (
