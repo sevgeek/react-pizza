623e0a4331c2460dc3b12ab2e420962b801736aa
Dmitry Shmatok <dev@sevgeek.ru>
2020-08-24 21:06:07 +0300

    Change Logo desc

--- src/components/Header/Logo/Logo.js
+++ src/components/Header/Logo/Logo.js
@@ -16,7 +16,7 @@ const Logo = ({ path }) => (
 				className='flex-item mr-m' />
 			<div id='logo-text' className='flex-item'>
 				<h1 className='mr-m'>React Pizza</h1>
-				<h5 className='txt-m m-0 p-0'>Реактивное веб-приложение интернет-магазина пиццы</h5>
+				<h5 className='txt-m m-0 p-0'>Реактивное веб-приложение для заказа пиццы</h5>
 			</div>
 		</div>
 	</Link>
