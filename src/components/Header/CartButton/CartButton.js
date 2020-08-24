e57e8c9e0d2806fce0c0372caef6f2f4ed197862
Dmitry Shmatok <dev@sevgeek.ru>
2020-08-24 09:30:18 +0300

    Styling CartButton

--- src/components/Header/CartButton/CartButton.js
+++ src/components/Header/CartButton/CartButton.js
@@ -10,25 +10,25 @@ import { Link } from 'react-router-dom'
  */
 const CartButton = ({ cart = [] }) => {
 
-	let orderedPizzas, totalPriceNode = undefined
+	let totalPrice, buttonCartText = undefined
 
 	if (cart.length > 0) {
 		// Определяем количество пицц в заказе
-		orderedPizzas = <div className='flex-item cart-button_count'>{cart.length}</div>
+		totalPrice = <div className='flex-item cart-button_count'>{cart.reduce((total, pizza) => total + pizza.price, 0)}&nbsp;₽</div>
 
 		// Определяем итоговую стоимость и формируем DOM-узел
-		totalPriceNode = <p className='flex-item cart-button_text'>{cart.reduce((total, pizza) => total + pizza.price, 0)} ₽</p>
+		buttonCartText = <p className='flex-item cart-button_text'>Корзина</p>
 	} else {
-		orderedPizzas = null
-		totalPriceNode = <p className='flex-item cart-button_text'>Корзина</p>
+		totalPrice = null
+		buttonCartText = <p className='flex-item cart-button_text'>Корзина</p>
 	}
 
 	return (
 		<Link to='/cart'>
 			<div className='cart-button pt-xs pb-xs pr-s pl-s txt-m'>
 				<div className='flexbox'>
-					{totalPriceNode}
-					{orderedPizzas}
+					{buttonCartText}
+					{totalPrice}
 				</div>
 			</div>
 		</Link>
