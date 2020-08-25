3d1a796ecedbb1cdcef8ecd8bae4e3868ac419db
Dmitry Shmatok <dev@sevgeek.ru>
2020-08-25 14:39:37 +0300

    Edit view total price in CartButton

--- src/components/Header/CartButton/CartButton.js
+++ src/components/Header/CartButton/CartButton.js
@@ -14,7 +14,7 @@ const CartButton = ({ cart = [] }) => {
 
 	if (cart.length > 0) {
 		// Определяем количество пицц в заказе
-		totalPrice = <div className='flex-item cart-button_count'>{cart.reduce((total, pizza) => total + pizza.price, 0)}&nbsp;₽</div>
+		totalPrice = <div className='flex-item cart-button_count'>{cart.reduce((total, pizza) => total + pizza.price * pizza.count, 0)}&nbsp;₽</div>
 
 		// Определяем итоговую стоимость и формируем DOM-узел
 		buttonCartText = <p className='flex-item cart-button_text'>Корзина</p>
