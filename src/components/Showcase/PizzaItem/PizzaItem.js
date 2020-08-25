366068a2c346396812a8dc54545f701d7dfb54f1
Dmitry Shmatok <dev@sevgeek.ru>
2020-08-25 09:14:01 +0300

    Create checkPizzaInTheCart

--- src/components/Showcase/PizzaItem/PizzaItem.js
+++ src/components/Showcase/PizzaItem/PizzaItem.js
@@ -63,4 +63,21 @@ const PizzaItem = ({ data, selectProps, addPizzaToCart, cart }) => {
 	)
 }
 
+/**
+* @name checkPizzaInTheCart
+* @description Проверка пиццы на наличие в корзине
+* @param {Object} pizza ID пиццы
+* @param {Array} cart Массив объектов пицц в корзине
+* @returns {Boolean} Наличие пиццы в корзине
+*/
+export function checkPizzaInTheCart(pizza, cart) {
+	if (cart !== undefined && cart.length > 0) {
+		// Search pizza
+		const searchPizza = cart.filter(item =>
+			item.type === pizza.type && item.id === pizza.id)
+		// Return result
+		return searchPizza.length > 0 ? true : false
+	} else return false
+}
+
 export default PizzaItem
