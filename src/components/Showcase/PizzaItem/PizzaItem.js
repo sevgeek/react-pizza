db5078fbb0b0c4152278d53e42e4220f095d3aa8
Dmitry Shmatok <dev@sevgeek.ru>
2020-08-26 11:48:04 +0300

    Transfer checkPizzaInTheCart

--- src/components/Showcase/PizzaItem/PizzaItem.js
+++ src/components/Showcase/PizzaItem/PizzaItem.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom'
 import PizzaProps from './PizzaProps/PizzaProps'
 
 /** Import functions */
-import transfromPizzaContentToText from '../../../functions'
+import { transfromPizzaContentToText, checkPizzaInTheCart } from '../../../functions'
 
 /**
  * @name PizzaItem
@@ -56,24 +56,6 @@ const PizzaItem = ({ data, selectProps, addPizzaToCart, cart }) => {
 			</div>
 		</div>
 	)
-}
-
-/**
-* @name checkPizzaInTheCart
-* @description Проверка пиццы на наличие в корзине
-* @param {Object} pizza ID пиццы
-* @param {Array} cart Массив объектов пицц в корзине
-* @returns {Boolean} Наличие пиццы в корзине
-*/
-export function checkPizzaInTheCart(pizza, cart) {
-	if (cart !== undefined && cart.length > 0) {
-		// Search pizza
-		const searchPizza = cart.filter(item =>
-			item.type === pizza.type && item.id === pizza.id)
-
-		// Return result
-		return searchPizza.length > 0 ? true : false
-	} else return false
 }
 
 export default PizzaItem
--- src/functions/index.js
+++ src/functions/index.js
@@ -4,6 +4,26 @@
  * @param {String} string Описание состава пиццы
  * @returns {String} Единое описание состава пиццы
  */
-export default function transfromPizzaContentToText(string) {
+export const transfromPizzaContentToText = (string) => {
 	return string[0].toUpperCase() + string.slice(1)
 }
+
+/**
+* @name checkPizzaInTheCart
+* @description Проверка пиццы на наличие в корзине
+* @param {Object} pizza ID пиццы
+* @param {Array} cart Массив объектов пицц в корзине
+* @returns {Boolean} Наличие пиццы в корзине
+*/
+export const checkPizzaInTheCart = (pizza, cart) => {
+	if (cart !== undefined && cart.length > 0) {
+		// Search pizza
+		const searchPizza = cart.filter(item =>
+			item.type === pizza.type && item.id === pizza.id)
+
+		// Return result
+		return searchPizza.length > 0 ? true : false
+	} else return false
+}
+
+export default transfromPizzaContentToText
